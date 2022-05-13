37463e7f14852553e642cebf7c1e255b1a9259f9
ericchi00 <ericchi00@gmail.com>
2022-05-13 16:34:13 -0700

    added in passport

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ import mongoose from 'mongoose';
 import session from 'express-session';
 import helmet from 'helmet';
 import compression from 'compression';
+import passport from 'passport';
 import __dirname from './dirname.js';
 
 import apiRouter from './routes/api.js';
+import * as authentication from './modules/authentication.js';
 
 const app = express();
 
@@ -20,8 +22,8 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(cors());
@@ -36,22 +38,34 @@ app.use(
 	})
 );
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'views')));
+// app.use(express.static(path.join(__dirname, 'views')));
 
 app.use(helmet());
 app.use(compression());
 
-app.use('/api', apiRouter);
+passport.use(authentication.local);
+passport.deserializeUser(authentication.deserializeUser);
+passport.serializeUser(authentication.serializeUser);
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', apiRouter);
+
+// catch 404 and forward to error handler
 app.use((req, res, next) => {
 	next(httpErrors(404));
 });
 
-// error handler
+app.use((req, res, next) => {
+	res
+		.status(404)
+		.json({ message: "We couldn't find what you were looking for 😞" });
+});
+
 app.use((err, req, res, next) => {
 	console.error(err.stack);
-	res.status(err.status || 500);
-	res.render('error');
+	res.status(500).json(err);
 });
 
 export default app;
