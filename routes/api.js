b761889cb6ee9c00ed497bb55baf79dd78856335
ericchi00 <ericchi00@gmail.com>
2022-05-13 16:35:22 -0700

    api routes

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import {
+	deletePost,
+	getAllPosts,
+	getPost,
+} from '../controllers/postcontroller.js';
+
+const router = Router();
+
+router.get('/', getAllPosts);
+
+router.get('/posts/:id', getPost);
+
+router.delete('/posts/:id', deletePost);
+
+export default router;
