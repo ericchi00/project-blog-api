81ca4ca09eada87ed0844aedd215a7fcf6f5c7e3
ericchi00 <ericchi00@gmail.com>
2022-05-18 15:20:34 -0700

    changed to populate username

--- controllers/messagecontroller.js
+++ controllers/messagecontroller.js
@@ -1,9 +1,10 @@
 import Message from '../models/message.js';
 import Comment from '../models/comment.js';
+import { body, validationResult } from 'express-validator';
 
 const getAllMessages = async (req, res, next) => {
 	try {
-		const list = await Message.find({ published: true }).exec();
+		const list = await Message.find().populate('username', 'username').exec();
 		res.json(list);
 	} catch (error) {
 		next(error);
@@ -22,6 +23,36 @@ const getMessage = async (req, res, next) => {
 	}
 };
 
+const postMessage = [
+	body('title')
+		.trim()
+		.isLength({ min: 3 })
+		.escape()
+		.withMessage('Title must be at least 3 characters'),
+	body('text')
+		.trim()
+		.isLength({ min: 3 })
+		.escape()
+		.withMessage('Text must be at least 3 characters'),
+	async (req, res, next) => {
+		try {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				res.json(errors);
+				return;
+			}
+			await Message.create({
+				username: req.body.id,
+				title: req.body.title,
+				text: req.body.text,
+			});
+			res.json(201);
+		} catch (error) {
+			next(error);
+		}
+	},
+];
+
 const deleteMessage = async (req, res, next) => {
 	try {
 		await Comment.deleteMany({ post: req.params.id }).exec();
@@ -32,4 +63,4 @@ const deleteMessage = async (req, res, next) => {
 	}
 };
 
-export { getAllMessages, getMessage, deleteMessage };
+export { getAllMessages, getMessage, deleteMessage, postMessage };
