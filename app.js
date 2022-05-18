b4a8d039d2cabd3ddaaf5bd3ec71f4ec63a776e1
ericchi00 <ericchi00@gmail.com>
2022-05-18 15:20:22 -0700

    added in error handler

--- app.js
+++ app.js
@@ -106,13 +106,19 @@ app.use((req, res, next) => {
 	next(httpErrors(404));
 });
 
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
 // error handler
 app.use((err, req, res, next) => {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	res.status(err || 500);
+	res.status(err.status || 500);
 	res.json(err);
 });
 
