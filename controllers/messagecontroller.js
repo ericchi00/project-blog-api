7fe48625720a9c9bd2d2a3de63f91226cdb12e3e
ericchi00 <ericchi00@gmail.com>
2022-05-18 13:22:31 -0700

    renamed to message

--- controllers/messagecontroller.js
+++ controllers/messagecontroller.js
@@ -1,19 +1,19 @@
-import Post from '../models/post.js';
+import Message from '../models/message.js';
 import Comment from '../models/comment.js';
 
-const getAllPosts = async (req, res, next) => {
+const getAllMessages = async (req, res, next) => {
 	try {
-		const list = await Post.find({ published: true }).exec();
+		const list = await Message.find({ published: true }).exec();
 		res.json(list);
 	} catch (error) {
 		next(error);
 	}
 };
 
-const getPost = async (req, res, next) => {
+const getMessage = async (req, res, next) => {
 	try {
 		const [post, comments] = await Promise.all([
-			Post.findById(req.params.id).exec(),
+			Message.findById(req.params.id).exec(),
 			Comment.find({ post: req.params.id }).exec(),
 		]);
 		res.json({ post, comments });
@@ -22,14 +22,14 @@ const getPost = async (req, res, next) => {
 	}
 };
 
-const deletePost = async (req, res, next) => {
+const deleteMessage = async (req, res, next) => {
 	try {
 		await Comment.deleteMany({ post: req.params.id }).exec();
-		await Post.findByIdAndDelete(req.params.id).exec();
+		await Message.findByIdAndDelete(req.params.id).exec();
 		res.json('success');
 	} catch (error) {
 		next(error);
 	}
 };
 
-export { getAllPosts, getPost, deletePost };
+export { getAllMessages, getMessage, deleteMessage };
