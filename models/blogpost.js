8a5d27c4b1d35f4887fed95d38bde15605abb64b
ericchi00 <ericchi00@gmail.com>
2022-05-19 15:17:49 -0700

    moved comment into blogpost arr

--- models/blogpost.js
+++ models/blogpost.js
@@ -2,18 +2,21 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
+const commentSchema = new Schema(
+	{
+		username: { type: Schema.Types.ObjectId, ref: 'User' },
+		text: { type: String, required: true, minlength: 2 },
+	},
+	{ timestamps: true }
+);
+
 const blogPostSchema = new Schema(
 	{
 		username: { type: Schema.Types.ObjectId, ref: 'User' },
 		title: { type: String, required: true },
 		text: { type: String, required: true },
 		date: { type: Date, default: Date.now },
-		comments: [
-			{
-				type: Schema.Types.ObjectId,
-				ref: 'Comment',
-			},
-		],
+		comments: [commentSchema],
 	},
 	{ timestamps: true }
 );
