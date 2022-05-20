baa1952b91ae53cdfca7041eaaad9578110826a8
ericchi00 <ericchi00@gmail.com>
2022-05-20 13:53:46 -0700

    removed comment schema back to model

--- models/blogpost.js
+++ models/blogpost.js
@@ -2,21 +2,13 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const commentSchema = new Schema(
-	{
-		username: { type: Schema.Types.ObjectId, ref: 'User' },
-		text: { type: String, required: true, minlength: 2 },
-	},
-	{ timestamps: true }
-);
-
 const blogPostSchema = new Schema(
 	{
 		username: { type: Schema.Types.ObjectId, ref: 'User' },
 		title: { type: String, required: true },
 		text: { type: String, required: true },
 		date: { type: Date, default: Date.now },
-		comments: [commentSchema],
+		comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
 	},
 	{ timestamps: true }
 );
