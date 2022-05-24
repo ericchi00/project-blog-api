d053b22cb83bc00251a9c4e454e3b57365ed48c1
ericchi00 <ericchi00@gmail.com>
2022-05-24 13:24:20 -0700

    removed session

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import logger from 'morgan';
 import mongoose from 'mongoose';
-import session from 'express-session';
 import helmet from 'helmet';
 import compression from 'compression';
 import passport from 'passport';
@@ -28,26 +27,10 @@ app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-	session({
-		secret: process.env.SESSION_SECRET,
-		resave: false,
-		saveUninitialized: true,
-		maxAge: 24 * 60 * 60 * 1000,
-	})
-);
 app.use(cookieParser());
 
 app.use(helmet());
 app.use(compression());
-
-app.use(
-	session({
-		secret: process.env.SESSION_SECRET,
-		resave: false,
-		saveUninitialized: true,
-	})
-);
 
 passport.use(
 	new LocalStrategy(async (username, password, done) => {
