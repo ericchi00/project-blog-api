53d245b7b815b3946a41032006a7c4d587b80562
ericchi00 <ericchi00@gmail.com>
2022-05-24 13:17:02 -0700

    cors update

--- app.js
+++ app.js
@@ -25,12 +25,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(logger('dev'));
-const corsOptions = {
-	origin: '*',
-	credentials: true,
-	optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
